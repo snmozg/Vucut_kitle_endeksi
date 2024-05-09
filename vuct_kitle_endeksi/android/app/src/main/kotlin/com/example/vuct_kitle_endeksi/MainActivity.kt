92f20bb1cd504d096b571b3f4c1ae2ca95d79dda
Sinem Özge ÖZKARTAL <sozgeozkartal@gmail.com>
2024-05-09 20:06:16 +0300

    Vücut Kitle Endeksi
    Android İçin

---
+++ vuct_kitle_endeksi/android/app/src/main/kotlin/com/example/vuct_kitle_endeksi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vuct_kitle_endeksi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ vuct_kitle_endeksi/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:vuct_kitle_endeksi/ana_sayfa.dart';
+
+void main() {
+  runApp(AnaUygulama());
+}
+class AnaUygulama extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:AnaSayfa(),
+
+
+    );
+  }
+}
